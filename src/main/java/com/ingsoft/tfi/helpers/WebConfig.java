bb5756e80806ce08c71abc5ceac69f6005efafe9
LautaroRomano <Lautarooyt837@gmail.com>
2024-11-24 18:35:44 -0300

    CORS Config

---
+++ src/main/java/com/ingsoft/tfi/helpers/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ingsoft.tfi.helpers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas las rutas de la API
+                .allowedOrigins("http://localhost:3000") // Dominio de tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los encabezados
+                .allowCredentials(true); // Permitir cookies
+    }
+}
